27107a83eb20ec3f5a32ac87c057c62314213d8a
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-06-09 16:38:59 +0000

    activesstorageインストール完了、deviceインストール完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "be2823e4608d42e7b3a2ed2851d5f2b7.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
